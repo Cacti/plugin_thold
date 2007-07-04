6b150950857739c5d9f83d95b26b65785d9a4a52
Jimmy Conner <cigamit@cacti.net>
2007-07-04 18:55:58 +0000

    Fix for baseline tholds not using cdefs
    git-svn-id: svn+ssh://repo.cacti.net/var/data/svnroot/cacti_plugins/thold/trunk@89 67214365-cc29-0410-bfbe-843132f0f3d7

--- thold-functions.php
+++ thold-functions.php
@@ -487,7 +487,7 @@ function &thold_check_baseline($rra_id, $ds, $ref_time, $ref_range, $current_val
 		// if($debug) echo "Baseline reference value not yet established!\n";
 		return -1; // Baseline reference value not yet established
 	}
-	
+	$current_value = get_current_value($rra_id,$ds);
 	$ref_value_max = round(max($ref_values));
 	$ref_value_min = round(min($ref_values));
 	
