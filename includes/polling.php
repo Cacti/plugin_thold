d701905eb25901ebd868f1495fcd3d6040bf776b
Pautina <pautina@kharkiv.net>
2018-01-25 16:23:45 +0200

    Add logging level for polling

--- includes/polling.php
+++ includes/polling.php
@@ -759,7 +759,7 @@ function thold_update_host_status() {
 				cacti_log('Host[' . $host['id'] . '] Hostname[' . $host['hostname'] . '] NOTE: Did not send a Device down email for \'' . $host['description'] . '\', disabled per Device setting!', true, 'THOLD');
 			} elseif ($alert_email != '') {
 				thold_mail($alert_email, '', $subject, $msg, '');
-//				acti_log('Host[' . $host['id'] . '] Hostname[' . $host['hostname'] . '] THOLD: Sent Email ' . $host['description'] . ' !', true, 'POLLER');
+//				acti_log('Host[' . $host['id'] . '] Hostname[' . $host['hostname'] . '] THOLD: Sent Email ' . $host['description'] . ' !', true, 'THOLD');
 			}
 
 			if ($alert_phone == '' && $host['thold_send_sms'] > 0) {
@@ -768,7 +768,7 @@ function thold_update_host_status() {
 				cacti_log('Host[' . $host['id'] . '] Hostname[' . $host['hostname'] . '] NOTE: Did not send a Device SMS for \'' . $host['description'] . '\', disabled per Device setting!', true, 'THOLD');
 			} elseif ($alert_phone != '') {
 				thold_sms($alert_phone, $subject);
-				cacti_log('Host[' . $host['id'] . '] Hostname[' . $host['hostname'] . '] THOLD: Sent SMS ' . $host['description'] . ' !', true, 'POLLER');
+				cacti_log('Host[' . $host['id'] . '] Hostname[' . $host['hostname'] . '] THOLD: Sent SMS ' . $host['description'] . ' !', true, 'THOLD', POLLER_VERBOSITY_MEDIUM);
 			}
 		}
 	}
