cb4313bc9e54051a60c4f82759ec52f509eff267
TheWitness <thewitness@cacti.net>
2020-12-28 21:30:48 -0500

    Remove deprecated utilities

--- locales/build_mo.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-for file in `ls -1 po/*.po`;do
-  ofile=$(basename --suffix=.po ${file})
-  echo "Converting $file to LC_MESSAGES/${ofile}.mo"
-  msgfmt ${file} -o LC_MESSAGES/${ofile}.mo
-done
